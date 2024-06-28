7c156513a93945cf4ed7338c5d8550a2ae25fbdd
Sanchit-Warang <95166732+Sanchit-Warang@users.noreply.github.com>
2024-06-28 13:14:32 +0530

    feat - :spakles Vercel Analytics added
    removed posthog :fire:
    tried pwa

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -4,78 +4,55 @@
   the workaround can be understood by the comments below
 */
 
-
 import { NextUIProvider } from '@nextui-org/react'
 import ReactQueryProviders from '@/lib/react-query/react-query-provider'
 import { Toaster } from 'react-hot-toast'
-import posthog from 'posthog-js'
-import { PostHogProvider } from 'posthog-js/react'
-
+import { Analytics } from '@vercel/analytics/react'
 // Next Session imports
-import { Session } from "next-auth";
+import { Session } from 'next-auth'
 import {
-    SessionProvider as NextSessionProvider,
-    getSession
-} from "next-auth/react";
-import { usePathname } from "next/navigation";
-import {
-    useCallback,
-    useLayoutEffect,
-    useState
-} from "react";
+  SessionProvider as NextSessionProvider,
+  getSession,
+} from 'next-auth/react'
+import { usePathname } from 'next/navigation'
+import { useCallback, useLayoutEffect, useState } from 'react'
 // Next Session imports done
 
-
-
-
-
-if (typeof window !== 'undefined') {
-  posthog.init(
-    process.env.NEXT_PUBLIC_POSTHOG_KEY
-      ? process.env.NEXT_PUBLIC_POSTHOG_KEY
-      : '',
-    {
-      api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
-    }
-  )
-}
-
 export function Providers({ children }: { children: React.ReactNode }) {
-
   // Next Session Fix
-  const [ session, setSession ] = useState<Session | null>(null);
-    const pathName = usePathname();
+  const [session, setSession] = useState<Session | null>(null)
+  const pathName = usePathname()
 
-    const fetchSession = useCallback(async () => {
-        try {
-            const sessionData = await getSession();
-            setSession(sessionData);
-        } catch (error) {
-            setSession(null);
+  const fetchSession = useCallback(async () => {
+    try {
+      const sessionData = await getSession()
+      setSession(sessionData)
+    } catch (error) {
+      setSession(null)
 
-            if (process.env.NODE_ENV === "development") {
-                console.error(error);
-            }
-        }
-    }, []);
+      if (process.env.NODE_ENV === 'development') {
+        console.error(error)
+      }
+    }
+  }, [])
 
-    useLayoutEffect(() => {
-        fetchSession().finally();
-    }, [fetchSession, pathName]);
+  useLayoutEffect(() => {
+    fetchSession().finally()
+  }, [fetchSession, pathName])
 
-    //Next Session fix end
-
+  //Next Session fix end
 
   return (
-    <NextSessionProvider session={session}>
-    <ReactQueryProviders>
-      <NextUIProvider>
-        <PostHogProvider client={posthog}>
-          {children}
-          <Toaster />
-        </PostHogProvider>
-      </NextUIProvider>
-    </ReactQueryProviders>
-    </NextSessionProvider>
+    <>
+      <NextSessionProvider session={session}>
+        <ReactQueryProviders>
+          <NextUIProvider>
+            {children}
+            <Toaster />
+          </NextUIProvider>
+        </ReactQueryProviders>
+      </NextSessionProvider>
+      <Analytics />
+    </>
   )
 }
